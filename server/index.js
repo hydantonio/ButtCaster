8e15e5fb25bbcba52c44d20d23c4c98ffa14e8cb
hydantonio <hyd.antonio@gmail.com>
2025-09-01 13:10:12 +0200

    fix: allow socket.io to serve client script

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ const path = require('path');
 const { connectIntiface, vibrateAll } = require('./intiface.js');
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer();
 const io = new SocketIO(server, { cors: { origin: '*' } });
+server.on('request', app);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'../public')));
@@ -47,11 +48,12 @@ io.on('connection', (socket)=>{
     await connectIntiface(url ?? state.intiface.url, state, io);
     socket.emit('intiface:devices', state.devices);
     io.emit('intiface:status', state.intiface);
+  });
   socket.on('tip', async ({amount = 100}) => {
     await handleTip(amount);
   });
 });
 
-app.get('*', (req,res)=> res.sendFile(path.join(__dirname,'../public/control.html')));
+app.get('/', (req,res)=> res.sendFile(path.join(__dirname,'../web/control.html')));
 
 const PORT = process.env.PORT || 3000; server.listen(PORT, ()=> console.log(`[ButtCaster] server on http://localhost:${PORT}`));
