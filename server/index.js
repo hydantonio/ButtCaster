55e4e6067b2dc68b467e8466ea5e51c03f6fbbe9
Antonio <hyd.antonio@gmail.com>
2025-09-01 14:42:14 +0200

    Fixed some double imports

--- server/index.js
+++ server/index.js
@@ -19,8 +19,6 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new SocketIO(httpServer, { cors: { origin: '*' } });
 const server = http.createServer(app);
-const server = http.createServer();
-const io = new SocketIO(server, { cors: { origin: '*' } });
 server.on('request', app);
 
 app.use(express.json());
@@ -71,9 +69,4 @@ app.get('/', (req,res)=> res.sendFile(path.join(__dirname,'../web/control.html')
 
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, ()=> console.log(`[ButtCaster] server on http://localhost:${PORT}`));
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, ()=> console.log(`[ButtCaster] server on http://localhost:${PORT}`));
 server.on('request', app);
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, ()=> console.log(`[ButtCaster] server on http://localhost:${PORT}`));
