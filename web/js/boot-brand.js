3315241b8feebd03843c36db3a4d1a0d26d64941
itsLogic <hyd.antonio@gmail.com>
2025-09-06 12:57:18 +0200

    Delete web directory

--- web/js/boot-brand.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', ()=>{
-  if(document.querySelector('.header-brand')) return;
-  const h=document.createElement('div'); h.className='header-brand fade-enter';
-  h.innerHTML = '<img src="/img/logo.svg" alt="ButtCaster"/><div class="header-title">ButtCaster Control</div><div class="badges"><div class="chip">v48</div></div>';
-  document.body.prepend(h); requestAnimationFrame(()=>h.classList.add('fade-enter-active'));
-});
